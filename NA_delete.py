7bf8db637f58efe0efd2ba9b9590a6bb35ffad1a
handabao <18862963156@163.com>
2026-04-29 00:42:52 +0800

    04_缺失值的删除操作.py

---
+++ NA_delete.py
@@ -0,0 +1,39 @@
+# 1.导包
+import pandas as pd
+
+# 2.加载数据
+train = pd.read_csv(r'data/titanic_train.csv')
+# 3.了解数据
+train.info()  # 详细信息
+print(train.shape)  # (892, 12)
+print('=' * 50)
+
+# 4.删除缺失值
+"""
+dropna(inplace=True): 直接在原有基础上删除
+dropna(): 默认是复制一份进行删除,原有的并没有被修改,只是返回了新的
+"""
+
+# 4.1 dropna() 默认how='any'直接删除有任意缺失值所在的行
+new_train1 = train.dropna()
+print(new_train1.shape)
+print('=' * 50)
+# 4.2 dropna(how='all') 如果整行都是缺失值那就删除,否则保留
+all_null_rows = train[train.isnull().all(axis=1)]
+print(f"全是缺失值的行数: {len(all_null_rows)}")
+print(all_null_rows)
+
+new_train2 = train.dropna(how='all')
+print(new_train2.shape)
+print('=' * 50)
+
+# 4.3 dropna(subset=[列名]) 指定重要列对应位置,有缺失值就删除所在行
+# 如果多个列,用列表包裹
+# new_train3 = train.dropna(subset=['Age', 'Cabin'])  # 默认how='any'
+new_train3 = train.dropna(subset='Age')  # 默认how='any'
+print(new_train3.shape)  # (714, 12)
+print('=' * 50)
+
+# 4.4 drop() 直接删除缺失值比较多的那一列
+new_train4 = train.drop('Cabin', axis=1)  # 1按照列删除
+print(new_train4.shape)
