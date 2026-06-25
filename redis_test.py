6d7757cabbab24296dc13c2f1ba70dcd1f7142f4
cuttingDust_47 <18862963156@163.com>
2026-06-25 17:47:46 +0800

    a test of using redis hash set

--- redis_test.py
+++ redis_test.py
@@ -6,12 +6,25 @@ r = redis.Redis(host="localhost", port=6379, db=0, decode_responses=True)
 # vl = r.get("name2")
 # print(vl)
 
-r.lpush("list1", "a", "b", "c")
-r.rpush("list1", "x")
+##########################################
+# r.lpush("list1", "a", "b", "c")
+# r.rpush("list1", "x")
+#
+# list1 = r.lrange("list1", 0, -1)
+# for val in list1:
+#    print(val)
+#
+###########################################
 
-list1 = r.lrange("list1", 0, -1)
-for val in list1:
-    print(val)
+r.hset("hash1", "a1", "a1")
+r.hset("hash1", "a2", "a2")
+r.hset("hash1", "a3", "a3")
+
+hash1 = r.hgetall("hash1")
+
+print(hash1)
+
+print(r.hlen("hash1"))
 
 
 r.close()
