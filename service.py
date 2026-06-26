2d97ee7378ab67306f5344db37697e3f7db0a100
cuttingDust_47 <18862963156@163.com>
2026-06-26 10:47:39 +0800

    update

--- service.py
+++ service.py
@@ -14,4 +14,21 @@ class MyThread(threading.Thread):
         self._stop_event = threading.Event()  # 用于控制线程停止
 
     def run(self) -> None:
+        while not self._stop_event.is_set():
+            self.sc.business()
+            time.sleep(1)
         return super().run()
+
+    def stop(self):
+        self._stop_event.set()
+
+
+# 使用
+if __name__ == "__main__":
+    t = MyThread()
+    t.start()
+
+    # 5 秒后停止
+    time.sleep(5)
+    t.stop()
+    t.join()
